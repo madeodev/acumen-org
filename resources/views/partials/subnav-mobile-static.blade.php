d654964b106c0a8545f5369fb48d0b99d0a08711
Sherif Fawzi <sherif.else@gmail.com>
2026-07-12 14:29:48 +0300

    Cache Flush 3

--- resources/views/partials/subnav-mobile-static.blade.php
+++ resources/views/partials/subnav-mobile-static.blade.php
@@ -21,7 +21,7 @@
       {{-- Text area --}}
       <div class="relative flex-1 bg-plum rounded-tr-10 rounded-br-10 overflow-hidden">
         <div class="absolute inset-0 bg-white/30 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none" aria-hidden="true"></div>
-        <div class="relative z-5 flex flex-col gap-2.5 p-5">
+        <div class="relative z-5 flex flex-col gap-2.5 p-5 max-w-[350px]">
           <p class="text-base font-semibold leading-extra-tight mb-0">Acumen appoints Carsten Stendevad as President and Chief Investment Officer</p>
           @svg('images.arrow-right', 'm-0.5 h-auto transition-transform duration-300 group-hover:translate-x-2.5')
         </div>
@@ -29,7 +29,7 @@
     </a>
 
     {{-- Navigation links --}}
-    <ul>
+    <ul class="flex flex-col gap-[4px]">
       <li>
         <x-button
           :link="['title' => 'Reports', 'url' => '#']"
