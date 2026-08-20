ab4eb01d0e06344ce282adef4cf50c904e4e1c54
Sherif Fawzi <sherif@madeomedia.com>
2026-08-20 17:40:13 +0300

    Revert "Background video accessibility"
    This reverts commit d3ff51bac5c77bd23a149610c48ca11a1ecb7952.

--- app/View/Components/LocalVideoLoop.php
+++ app/View/Components/LocalVideoLoop.php
@@ -6,8 +6,6 @@ use Illuminate\View\Component;
 
 class LocalVideoLoop extends Component
 {
-    private static int $instanceCount = 0;
-
     /**.
      *
      * @var string
@@ -25,24 +23,6 @@ class LocalVideoLoop extends Component
      * @var string
      */
     public $accessibleLabel;
-
-    /**.
-     *
-     * @var string
-     */
-    public $videoId;
-
-    /**.
-     *
-     * @var string
-     */
-    public $playAriaLabel;
-
-    /**.
-     *
-     * @var string
-     */
-    public $pauseAriaLabel;
 
     /**
      * Create a new component instance.
@@ -64,13 +44,9 @@ class LocalVideoLoop extends Component
         public bool $decorative = true,
     )
     {
-        self::$instanceCount++;
-        $this->videoId = 'video-' . self::$instanceCount;
         $this->thumbnail_url = empty($placeholder) ? '' : wp_get_attachment_image_url($placeholder, 'medium_large');
         $this->caption = $this->getVideoDescription($webm, $mp4);
         $this->accessibleLabel = $this->getAccessibleLabel();
-        $this->playAriaLabel = __('Play video', 'video');
-        $this->pauseAriaLabel = __('Pause video', 'video');
     }
 
     /**
--- resources/views/components/local-video-loop.blade.php
+++ resources/views/components/local-video-loop.blade.php
@@ -3,6 +3,7 @@
 
     <figure
         x-data="localVideo({{$preload}})"
+        x-id="['video']"
         x-intersect.margin.400px.once="load"
         x-intersect:enter="videoIsVisible"
         x-intersect:leave="videoIsNotVisible"
@@ -11,13 +12,13 @@
         <video 
             autoplay muted loop playsinline 
             x-ref="video"
-            id="{{ $videoId }}"
+            :id="$id('video')"
             class="absolute inset-0 overflow-hidden h-full w-full object-cover"
             @if(!empty($thumbnail_url))
                 poster="{{$thumbnail_url}}"
             @endif
             @if(!empty($accessibleLabel))
-                aria-label="{{ e($accessibleLabel) }}"
+                aria-label="{{ $accessibleLabel }}"
             @elseif($decorative)
                 aria-hidden="true"
                 role="presentation"
@@ -62,11 +63,11 @@
             type="button"
             class="absolute inset-0 focus-visible:-outline-offset-4 focus-visible:outline-white w-full"
             @click="playPause()"
-            aria-controls="{{ $videoId }}"
-            aria-pressed="{{ $preload ? 'true' : 'false' }}"
-            aria-label="{{ e($preload ? $pauseAriaLabel : $playAriaLabel) }}"
             :aria-pressed="isPlaying"
-            :aria-label="isPlaying ? @json($pauseAriaLabel) : @json($playAriaLabel)"
+            :aria-controls="$id('video')"
+            :aria-label="isPlaying ?
+                '{!! __('Pause video', 'video') !!}' :
+                '{!! __('Play video', 'video') !!}'"
             :title="isPlaying ?
                 '{!! __('Click to pause video', 'video') !!}' :
                 '{!! __('Click to play video', 'video') !!}'"
